503b14003e2fb5358a07ea05c5d9e9630abe000a
Michail Tsibrankov <mishka.barman@gmail.com>
2022-02-28 21:50:46 +0300

    First commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,29 +1,46 @@
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 
-public class MainTest extends BaseTest
-{
+public class MainTest extends BaseTest {
     @Test
     public void getFirstEmail() {
         requestSpecification
                 .when()
-                    .get()
+                .get()
                 .then()
-                    .statusCode(200)
+                .statusCode(200)
                 .body("data[0].first_name", equalTo("George"))
                 .body("data[0].last_name", equalTo("Bluth"))
                 .body("data[0].email", equalTo("george.bluth@reqres.in"));
     }
+
     @Test
     public void getSecondEmail() {
-        requestSpecification
-                .when()
-                    .get()
-                    .then()
-                        .statusCode(200)
-                .body("data.find{it.email=='michael.lawson@reqres'}.first_name",
-                        equalTo("Michael"));
+       findPage();
     }
 
+    private void findPage() {
+        int total_pages = 12;
+        for (int pages = 1; pages < total_pages; pages++) {
+            Response response = given()
+                    .baseUri("https://reqres.in/api")
+                    .basePath("/users")
+                    .contentType(ContentType.JSON)
+                    .param("page", pages)
+                    .when()
+                    .get()
+                    .then()
+                    .statusCode(200)
+                    .body("data[0].email", equalTo("michael.lawson@reqres.")).extract().response();
+
+            JsonPath jp = new JsonPath(response.asString());
+        }
+    }
 }
